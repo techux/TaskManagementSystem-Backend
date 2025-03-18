c76d4abd3d0a3746d3660b57fb78ad82b32fc5d2
techux <connect.world12345@gmail.com>
2025-03-18 18:32:10 +0530

    Added: Task DB Model, Updated: changed user model

---
+++ models/task.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("task", taskSchema);
+
+module.exports = Task;
--- models/user.model.js
+++ models/user.model.js
@@ -1,39 +1,30 @@
-const { validate } = require('email-validator');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator: e => validate(e),
-            message: props => `${props.value} is not a valid email!`
-        }        
+      type: String,
+      required: true,
+      unique: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    role: {
-        type: String,
-        enum: ['admin', 'user'],
-        default: 'member'
-    },},
-{
+  },
+  {
     timestamps: true,
-    versionKey: false
-})
-
+  }
+);
 
 const User = mongoose.model("user", userSchema);
 
